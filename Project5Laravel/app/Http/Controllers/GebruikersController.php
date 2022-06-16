ab98f015886008dcc2c74470e7ad67d80ceb555b
Robin Hendrikx <PS221166@summacollege.nl>
2022-06-16 09:48:35 +0200

    Gebruikers added

---
+++ Project5Laravel/app/Http/Controllers/GebruikersController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gebruikers;
+use Illuminate\Http\Request;
+
+class GebruikersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Gebruikers  $gebruikers
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gebruikers $gebruikers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Gebruikers  $gebruikers
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Gebruikers $gebruikers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Gebruikers  $gebruikers
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gebruikers $gebruikers)
+    {
+        //
+    }
+}
