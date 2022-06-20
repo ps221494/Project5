64352ecf1370460fbfba4d5643ccee4a05c07d9d
Robin Hendrikx <PS221166@summacollege.nl>
2022-06-20 10:31:19 +0200

    user api route updated

--- Project5Laravel/routes/api.php
+++ Project5Laravel/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GebruikersController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +15,9 @@ use App\Http\Controllers\GebruikersController;
 |
 */
 
-Route::apiResource('gebruikers', GebruikersController::class);
+Route::apiResource('users', UserController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-    
+
 });
