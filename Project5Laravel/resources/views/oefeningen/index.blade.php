fcdaed2dd2091e9548a47cdd0dc1f727a5ac647b
Delano van Londen <ps221494@summacollege.nl>
2022-06-21 13:08:29 +0200

    Delete excersize

--- Project5Laravel/resources/views/oefeningen/index.blade.php
+++ Project5Laravel/resources/views/oefeningen/index.blade.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Oefeningen</title>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </head>
 
 <body onload="laad()">
@@ -29,27 +30,31 @@
     "use strict"
     const apiBasis = "http://localhost:8000/api/"
     const apiExcersizes = apiBasis + "excersizes/"
-
+    
+  //  const axios = require('axios');
 //let functies = []
 //let json = null
 
     const laadExcersizes = async () => {
-       const respone = await axios.get(apiExcersizes, {
+
+       const response = await axios.get(apiExcersizes, {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             }
        })
 
-       if   (response.status != 200) {
-            console.log("Er is iets fout gegaan")
-            return
-        }
-        const response = await fetch(apiExcersizes)
-        
+       console.log('response', response)
+       if (response.status != 200) {
+			alert(`Fout bij het lezen van de functies: ${response.status} (${response.statusText})`)
+			return
+		}
+       const json = await response.data
+        console.log(json)
+
         let tabelInhoud = ''
-        json.map(el => tabelInhoud +=
-            `<tr><td>${el.id}</td><td>${el.Name}</td><td>${el.Description}</td><td>${el.Image}</td><td onclick="verwijder(${el.id})">x</td></tr>`)
+        json.data.map(el => tabelInhoud +=
+            `<tr><td>${el.id}</td><td>${el.Name}</td><td>${el.Description}</td><td>${el.Image}</td><button onclick="verwijder(${el.id})">x</button></tr>`)
         document.querySelector("#tabelInhoudExcersizes").innerHTML = tabelInhoud
        }
         /*
@@ -66,6 +71,17 @@
 
     const laad = async () => {
         laadExcersizes()
+    }
+
+    const verwijder = async (id) => {
+        const response = await axios.delete(apiExcersizes + id, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        console.log(response)
+        laad();
     }
 </script>
 
