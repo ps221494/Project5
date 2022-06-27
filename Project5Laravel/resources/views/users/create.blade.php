784ce52916a182371f40b5bb23c1a21f67224914
Robin Hendrikx <PS221166@summacollege.nl>
2022-06-27 14:15:54 +0200

    Create user start

--- Project5Laravel/resources/views/users/create.blade.php
+++ Project5Laravel/resources/views/users/create.blade.php
@@ -1,9 +1,46 @@
 <!DOCTYPE html>
-<html>
-    <head>
+<html lang="en">
 
-    </head>
-    <body>
-        <h1>This is a test</h1>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Create Users</title>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th scope="col">id</th><td><p>test</p></td>
+            <th scope="col">Name</th>
+        </tr>
+        <tbody id="tabelInhoudUsers" />
+    </table>
+
+    <div class="pull-right">
+        <a class="btn btn-primary" onclick="create()" href="{{ route('users.index') }}"> Create new user</a>
+    </div>
+
+</body>
+
+<script>
+    "use strict"
+    const apiBasis = "http://localhost:8000/api/"
+    const apiUsers = apiBasis + "users/"
+
+    const create = async () => {
+        const response = await axios.create(apiUsers + id, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        console.log(response)
+    }
+
+</script>
+
 </html>
